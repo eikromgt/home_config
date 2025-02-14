c6bede564d32c874c4ae1b22667c3f2c25aaf7e4
Luo Leifei <eikromgt@outlook.com>
2025-02-14 23:57:38 +0800

    Added installing of kitty theme

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ echo "Start install configuration files"
 
 rsync -av  home/ ~/
 
-echo "Installing yazi themes"
+echo "Installing kitty theme"
+git clone https://github.com/wdomitrz/kitty_gruvbox_theme.git ~/.config/kitty/kitty_gruvbox_theme
+
+echo "Installing yazi theme"
 ya pack -a bennyyip/gruvbox-dark
 
-echo "Installing zathura themes"
+echo "Installing zathura theme"
 git clone https://github.com/eastack/zathura-gruvbox.git ~/.config/zathura/zathura-gruvbox
 
 echo "Finished!"
