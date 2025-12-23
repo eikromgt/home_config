cd1affbf84268db52fbd9738b2f58ff2ecbed3a2
Luo Leifei <eikromgt@outlook.com>
2025-12-23 19:52:08 +0800

    add cpp TreeNode definition for leetcode

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -71,6 +71,15 @@ local leetcodeCppBeforeInjection = [[
 #include <mutex>
 #include <ranges>
 using namespace std;
+
+struct TreeNode {
+ int val;
+ TreeNode *left;
+ TreeNode *right;
+ TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 ]]
 
 local leetcodePythonBeforeInjection = [[
