cdfe421cf5b5bfeb59f7c7f2c7713dbebe11d168
eikromgt <85564672+eikromgt@users.noreply.github.com>
2026-09-11 12:02:53 +0800

    Enhance entrypoint.sh for user group management
    Added functionality to manage user groups based on HOST_REPO and COLOR_MODE variables.

--- home/repo/dockerfile/dev/entrypoint.sh
+++ home/repo/dockerfile/dev/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 if [[ -S /var/run/docker.sock ]]; then
-    GID=$(stat -c %g /var/run/docker.sock)
-    groupadd -g $GID hostdocker 2>/dev/null || true
-    usermod -aG $GID beanopy
+    DOCKER_GID=$(stat -c %g /var/run/docker.sock)
+    groupadd -g $DOCKER_GID host_docker 2>/dev/null || true
+    usermod -aG $DOCKER_GID beanopy
+fi
+
+if [[ "$HOST_REPO" && -d "$HOST_REPO" ]]; then
+    REPO_GID=$(stat -c %g "$HOST_REPO")
+    groupadd -g $REPO_GID host_user 2>/dev/null || true
+    usermod -aG $REPO_GID beanopy
+    [[ -e /home/beanopy/hrepo ]] || ln -s "$HOST_REPO" /home/beanopy/hrepo
+fi
+
+if [[ "$COLOR_MODE" == "light"  ]]; then
+    sed -i 's/vim.opt.background      = "dark"/vim.opt.background      = "light"/' /home/beanopy/.config/nvim/init.lua
+    sed -i 's/theme = "gruvbox_dark_hard"/theme = "gruvbox_light_hard"/' /home/beanopy/.config/helix/config.toml
 fi
 
 exec "$@"
